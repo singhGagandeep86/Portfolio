05a52f710ac89c970678c68eae38476efe69d4bf
GagandeepSingh <gagandeep.201119@gmail.com>
2025-02-22 19:49:09 +0100

    added background on click in navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
@@ -8,7 +8,6 @@ import { MySkillComponent } from "./my-skill/my-skill.component";
 import { ProjectComponent } from "./project/project.component";
 import { ReferencesComponent } from './references/references.component';
 import { ContactComponent } from './contact/contact.component';
-import { ViewportScroller } from '@angular/common';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @Component({
@@ -18,35 +17,5 @@ import { NavBarComponent } from "./nav-bar/nav-bar.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-
-  @ViewChild('navBar') navBar?: ElementRef;
-
-
-  title = 'Portfolio';
-  scrollPos = [0, 0];
-
-  constructor (public scroller: ViewportScroller){}
-
-  ngOnInit(): void {
-    this.getScrollerPos();
-  }
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    this.getScrollerPos();
-  }
-
-
-  getScrollerPos() {
-    this.scrollPos = this.scroller.getScrollPosition();
-    if (this.scrollPos[1] > 758) {
-      console.log(`test`);
-      console.log(this.scrollPos[1]);
-      if (this.navBar) {
-        this.navBar.nativeElement.style.position = "fixed";
-      }
-
-    }
-  }
+export class AppComponent {
 }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
+
+
 export class NavBarComponent {
+
+  aboutMeStatus: boolean = false;
+  mySkillsStatus: boolean = false
+  projectsStatus: boolean = false;
+  contactStatus: boolean = false
+
+  tabClicked(event: Event) {
+    const clickedElement = event.target as HTMLElement;
+
+    if (clickedElement.id == 'aboutMe') {
+      this.aboutMeStatus = true;
+      this.mySkillsStatus = false;
+      this.projectsStatus = false;
+      this.contactStatus = false;
+    }
+
+    if (clickedElement.id == 'mySkills') {
+      this.aboutMeStatus = false;
+      this.mySkillsStatus = true;
+      this.projectsStatus = false;
+      this.contactStatus = false;
+    }
+
+    if (clickedElement.id == 'projects') {
+      this.aboutMeStatus = false;
+      this.mySkillsStatus = false;
+      this.projectsStatus = true;
+      this.contactStatus = false;
+    }
+
+    if (clickedElement.id == 'contact') {
+      this.aboutMeStatus = false;
+      this.mySkillsStatus = false;
+      this.projectsStatus = false;
+      this.contactStatus = true;
+    }
+
+  }
 
 }
